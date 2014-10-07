b104180afbaac89cd94a98fe81e701df45b29f0c
Markus Silvan <markus.silvan@iki.fi>
2014-10-07 23:03:46 +0300

    Added application skeleton. Just an empty window and an icon. The window is centered to the screen.

---
+++ src/ditz-gui.py
@@ -0,0 +1,54 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Ditz-gui
+
+A GUI frontend for Ditz issue tracker
+
+Ditz is a simple, light-weight distributed issue tracker designed
+to work with distributed version control systems like git, darcs,
+Mercurial, and Bazaar. It can also be used with centralized systems
+like SVN. Ditz maintains an issue database directory on disk, with
+files written in a line-based and human-editable format. This
+directory can be kept under version control, alongside project code.
+"""
+
+import sys
+from PyQt4 import QtGui
+
+
+class DitzGui(QtGui.QWidget):
+    """
+    The main window
+    """
+    def __init__(self):
+        """
+        Initialize user interface
+        """
+        super(DitzGui, self).__init__()
+
+        self.resize(500, 350)
+        self.center()
+        self.setWindowTitle('Ditz GUI')
+        self.setWindowIcon(QtGui.QIcon('../graphics/ditz_gui_icon.png'))
+        self.show()
+
+    def center(self):
+        """
+        Center the window to screen
+        """
+        rect = self.frameGeometry()
+        desktop_center = QtGui.QDesktopWidget().availableGeometry().center()
+        rect.moveCenter(desktop_center)
+        self.move(rect.topLeft())
+
+
+def main():
+    app = QtGui.QApplication(sys.argv)
+    ditz_gui = DitzGui()
+
+    sys.exit(app.exec_())
+
+if __name__ == '__main__':
+    main()
